0643f818a1c42791dc7e920aca35e61713bd0922
UMMANCODE <153718950+UMMANCODE@users.noreply.github.com>
2024-09-04 02:01:44 +0400

    Update IUserHouseService.cs

--- Final_Business/Services/Interfaces/IUserHouseService.cs
+++ Final_Business/Services/Interfaces/IUserHouseService.cs
@@ -1,10 +1,10 @@
-﻿namespace Final_Business.Services.Interfaces;
+namespace Final_Business.Services.Interfaces;
 public interface IUserHouseService {
   public Task<BaseResponse> Create(UserHouseCreateDto createDto);
   public Task<BaseResponse> GetPaginated(int pageNumber = 1, int pageSize = 1);
   public Task<BaseResponse> GetAll();
   public Task<BaseResponse> GetById(int id);
   public Task<BaseResponse> Update(int id, UserHouseUpdateDto updateDto);
-  public Task<BaseResponse> Delete(int id);
+  public Task<BaseResponse> Delete(int id); 
   public Task<BaseResponse> Filter(PropertyStatus? status = null, PropertyType? type = null, PropertyState? state = null);
 }
